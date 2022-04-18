089dee50310d9a1458b05b8e119a24c825aefacc
moni-que <monicahjustus@gmail.com>
2022-04-18 18:41:15 +0300

    import eventEmitter

--- src/app/search-user/search-user.component.ts
+++ src/app/search-user/search-user.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output,EventEmitter } from '@angular/core';
+import { UserProfileComponent } from '../user-profile/user-profile.component';
+import { SearchService } from '../search-service/search.service';
 
 @Component({
   selector: 'app-search-user',
@@ -7,7 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchUserComponent implements OnInit {
 
+  public userField!:string;
+  @Output() searchingResult = new EventEmitter<any>()
+  searchingUser(){
+    this.searchingResult.emit(this.userField);
+  }
+
   constructor() { }
+
 
   ngOnInit(): void {
   }
