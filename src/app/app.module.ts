7103150307990ba807973ac7e9ec009b1eacf142
moni-que <monicahjustus@gmail.com>
2022-04-18 07:55:20 +0300

    generate component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RepositoryComponent } from './repository/repository.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RepositoryComponent,
+    UserProfileComponent
   ],
   imports: [
     BrowserModule,
