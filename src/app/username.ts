a30291dd759046385bf64976808f99d8e70e9bf7
moni-que <monicahjustus@gmail.com>
2022-04-18 17:31:24 +0300

    add interpolation

--- src/app/username.ts
+++ src/app/username.ts
@@ -1,3 +1,3 @@
 export class Username {
-    constructor(public avatar_url:string, public name: string, public_repos: number,public url:string){}
+    constructor(public avatar_url:string, public name: string, public_repositories: number,public html_url:string){}
 }
