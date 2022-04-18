50e35064c2a884744d2b44b2fbaee8ccee145fa2
moni-que <monicahjustus@gmail.com>
2022-04-18 15:52:24 +0300

    import repository and username

--- src/app/search-service/search.service.ts
+++ src/app/search-service/search.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { Username } from '../username';
+import { Repository } from '../repository';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchService {
+  username!:Username;
+  repo!:Repository;
 
-  constructor() { }
+  constructor(private http:HttpClient) { 
+    this.username = new Username("","",0,"")
+    this.repo = new Repository("","","","",new Date())
+  }
 }
